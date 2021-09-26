26b1af1807ea56c3563296d68747a125a2a01150
Hakan Özoğlu <hakan.ozoglu@glomil.com>
2021-09-26 18:09:24 +0300

    log environent

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(express.json());
   app.get("/", function (request, response) {
     response.send("Welcome to Yunniq API 0.0.1!");
   });
-
+console.log("environment",process.env)
   const server = app.listen(process.env.APP_PORT, () => {
     asciify("Korri", { font: "epic", color: "green" }, function (err, res) {
       console.log(res);
