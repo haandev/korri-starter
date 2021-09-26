325381dcb4aa1be5764ac5b71f8d08c920b33b50
Hakan Özoğlu <hakan.ozoglu@glomil.com>
2021-09-26 18:43:13 +0300

    change listening

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 const express = require("express");
 
-const cors = require("cors");
+/*const cors = require("cors");
 const cookieParser = require('cookie-parser')
 
 const asciify = require("asciify");
 const morgan = require("morgan");
 console.log("environment",process.env)
-const { connect, sync } = require("./init-connection");
+const { connect, sync } = require("./init-connection");*/
 
 const app = express();
-
+/*
 app.use(morgan("dev"));
 app.use(cors());
 app.use(cookieParser())
 
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json());*/
 
 (async () => {
-  await connect();
+/*  await connect();
 
   const routers = require("./init-router")(app);
   //require("./init-swagger")(app, routers);
-  await sync();
+  await sync();*/
 
   app.get("/", function (request, response) {
     response.send("Welcome to Yunniq API 0.0.1!");
