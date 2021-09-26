e2f25a18eed72f645c3ae87b6675390f94643dc2
Hakan Özoğlu <hakan.ozoglu@glomil.com>
2021-09-26 22:10:34 +0300

    configure ssl

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const https = require('https');
+const http = require('http');
 const express = require("express");
 
 const cors = require("cors");
@@ -5,6 +7,7 @@ const cookieParser = require('cookie-parser')
 
 const asciify = require("asciify");
 const morgan = require("morgan");
+const fs = require("fs");
 const { connect, sync } = require("./init-connection");
 
 const app = express();
@@ -26,15 +29,22 @@ app.get("/", function (request, response) {
   await sync();
 
 
-  const port = process.env.PORT || 3000
-  const server = app.listen(port , () => {
-    asciify("Korri", { font: "epic", color: "green" }, function (err, res) {
-      console.log(res);
-      console.log(
-        `\nWelcome to Yunniq API 0.0.1! Listening on port ${server.address().port}` +
-          `\nhttp://${server.address().address}:${server.address().port}` +
-          `\nRunning on environment: ${process.env.NODE_ENV}`
-      );
-    });
+  asciify("Korri", { font: "epic", color: "green" }, function (err, res) {
+    console.log(res);
   });
+
+
+
+  const options = {
+    key: fs.readFileSync('./private.key'),
+    cert: fs.readFileSync('./certificate.crt'),
+  };
+
+  http.createServer(app).listen(80);
+  https.createServer(options, app).listen(443);
+  console.log(
+      `\nWelcome to Yunniq API 0.0.1! Listening on port 80 and 443` +
+      `\nRunning on environment: ${process.env.NODE_ENV}`
+  );
+
 })();
